98832aa1c048924826c749299006b2bc86e8fc5f
Luis Rod <luis29.rod+dev@gmail.com>
2025-10-13 09:31:01 -0400

    feat: add function to validate connection with GPT model

--- src/summarizer/main.py
+++ src/summarizer/main.py
@@ -1,5 +1,6 @@
 import os
 from summarizer.open_ai_client import validate_api_key
+from summarizer.open_ai_client import validation_gpt_chat
 from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
@@ -8,3 +9,5 @@ from bs4 import BeautifulSoup
 # Validate the API key before proceeding
 validate_api_key()
 
+# validate gpt chat
+validation_gpt_chat()
--- src/summarizer/open_ai_client.py
+++ src/summarizer/open_ai_client.py
@@ -2,17 +2,14 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
+ # Load environment variables in a file called .env
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
 
 def validate_api_key():
     """Validate the OpenAI API key from environment variables."""
 
-    # Load environment variables in a file called .env
-    load_dotenv(override=True)
-    api_key = os.getenv('OPENAI_API_KEY')
-
-
     # Check the key validity
-
     if not api_key:
         print("No API key was found - please head over to the troubleshooting notebook in this folder to identify and fix it!")
     elif not api_key.startswith("sk-proj-"):
@@ -22,3 +19,13 @@ def validate_api_key():
     else:
         print("API key found and looks good so far!")
 
+
+# create an instance of the OpenAI client
+openai = OpenAI()
+
+
+# create a message and get a response from the model
+def validation_gpt_chat():
+    message = "Hello, GPT! This is my first ever message to you! Hi!"
+    response = openai.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user", "content":message}])
+    print(response.choices[0].message.content)
