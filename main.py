edb874aca39ab4040246071e3bcba73b1b37c71b
Luis Rod <luis29.rod+dev@gmail.com>
2025-10-17 10:15:10 -0400

    refactor(summarizer): move main.py to root level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-from summarizer.open_ai_client import validate_api_key
-from summarizer.open_ai_client import validation_gpt_chat
+from src.summarizer.open_ai_client import validate_api_key
+from src.summarizer.open_ai_client import validation_gpt_chat
 from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
